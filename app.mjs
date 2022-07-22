5ae34e07b385250bcf89fd407897a52cbb106e69
Olumese Steve-John <73387073+JumaCodes@users.noreply.github.com>
2022-07-22 16:28:52 +0000

    Added all input Feed and Button in the Html file and done a little bit of Css and Javascript. to be continued.

--- app.mjs
+++ app.mjs
@@ -1,17 +1,38 @@
-function startApp() {
-    // Your entire app should not necessarily be coded inside this 
-    // single function (though there's no penalty for that), 
-    // so create and use/call additional functions from here
-  
-    // pls remove the below and make some magic in here!
-    console.log('make magic in here!');
-  
-    const header = document.querySelector('h2');
-    if(header) {
-      header.textContent = 'make some magic here!!';
+ function countOccurences(string, word) {
+    return string.split(word).length - 1;
+  }
+
+
+  function redactWords() {
+    var badWords = document.getElementById("searchtext").value;
+    var text = document.getElementById("query").value;
+    var wordsMatched = countOccurences(badWords, text)
+    var query = new RegExp("(\\b" + text + "\\b)", "gim");
+    var newWords = document.getElementById("newwords").value;
+    if (query.test(badWords) === true) {
+      var enew = badWords.replace(/(<span>|<\/span>)/igm, "");
+      document.getElementById("searchtext").innerHTML = enew;
+      var newe = enew.replace(query, `${newWords}`);
     }
-  };
-  
+    document.getElementById("searchtext").value = newe;
+
+
+
+    document.getElementById("searchedwords").textContent = "total number of scanned words: " + " " + badWords.length;
+    document.getElementById("matchedwords").textContent = "total number of matched words: " + " " + wordsMatched;
+
+    document.getElementById("scrambledwords").textContent = "total number of scrambled character: " + " " + wordsMatched.length;
+  }
+
+function startApp(e) {
+  e.preventDefault();
+  // Your entire app should not necessarily be coded inside this 
+  // single function (though there's no penalty for that), 
+  // so create and use/call additional functions from here
+
+  // pls remove the below and make some magic in here!
+  document.getElementById("btn").addEventListener("click", redactWords)
+
   // ======= DO NOT EDIT ============== //
   export default startApp;
   // ======= EEND DO NOT EDIT ========= //
