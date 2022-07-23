24c77467469fefa0c805c29e3f8cdc964df951ed
Olumese Steve-John <73387073+JumaCodes@users.noreply.github.com>
2022-07-23 21:09:52 +0000

    we have successfully mearnt all the requirement of the Redactr App AlsSchool holliday assignment challenge. Hurray Go Circle 81.

--- app.mjs
+++ app.mjs
@@ -1,29 +1,3 @@
- function countOccurences(string, word) {
-    return string.split(word).length - 1;
-  }
-
-
-  function redactWords() {
-    var badWords = document.getElementById("searchtext").value;
-    var text = document.getElementById("query").value;
-    var wordsMatched = countOccurences(badWords, text)
-    var query = new RegExp("(\\b" + text + "\\b)", "gim");
-    var newWords = document.getElementById("newwords").value;
-    if (query.test(badWords) === true) {
-      var enew = badWords.replace(/(<span>|<\/span>)/igm, "");
-      document.getElementById("searchtext").innerHTML = enew;
-      var newe = enew.replace(query, `${newWords}`);
-    }
-    document.getElementById("searchtext").value = newe;
-
-
-
-    document.getElementById("searchedwords").textContent = "total number of scanned words: " + " " + badWords.length;
-    document.getElementById("matchedwords").textContent = "total number of matched words: " + " " + wordsMatched;
-
-    document.getElementById("scrambledwords").textContent = "total number of scrambled character: " + " " + wordsMatched.length;
-  }
-
 function startApp(e) {
   e.preventDefault();
   // Your entire app should not necessarily be coded inside this 
@@ -31,7 +5,36 @@ function startApp(e) {
   // so create and use/call additional functions from here
 
   // pls remove the below and make some magic in here!
+  function countOccurences(string, word) {
+    return string.split(word).length - 1;
+  }
+
+
+  function redactWords() {
+    let badWords = document.getElementById("searchtext").value;
+    let text = document.getElementById("query").value;
+    let wordsMatched = countOccurences(badWords, text)
+    let query = new RegExp("(\\b" + text + "\\b)", "gim");
+    let newWords = document.getElementById("newwords").value;
+    
+    if (query.test(badWords) === true) {
+      let enew = badWords.replace(/(<span>|<\/span>)/igm, "");
+      document.getElementById("searchtext").innerHTML = enew;
+      var newe = enew.replace(query, `${newWords}`);
+    }
+    document.getElementById("searchtext").value = newe;
+    
+
+
+    let total = document.getElementById("total");
+  total.classList.add("totalWords");
+    document.getElementById("searchedwords").textContent = "Total number of scanned words: " + " " + badWords.length;
+    document.getElementById("matchedwords").textContent = "Total number of matched words: " + " " + wordsMatched;
+
+    
+  }
   document.getElementById("btn").addEventListener("click", redactWords)
+}
 
   // ======= DO NOT EDIT ============== //
   export default startApp;
